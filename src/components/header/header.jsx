7bb9270b0635d6d8c290ebb8cfc52961a0baa25b
Shivangi Chaurasia <56307595+shivangi3001@users.noreply.github.com>
2022-06-08 21:27:29 +0530

    first commit

---
+++ src/components/header/header.jsx
@@ -0,0 +1,15 @@
+import './header.css';
+
+function Header() {
+    return (
+        <div id="home" className="container header-container">
+            <h3>
+                <p>I<span>'</span>m</p>
+                Shivangi
+                <h2> MERN Stack developer </h2>
+            </h3>
+        </div>
+    );
+}
+
+export default Header;
