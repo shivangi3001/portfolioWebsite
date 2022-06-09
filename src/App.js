35953f6419bcb1fca213bde8cdc9c501a8ec062d
Shivangi Chaurasia <56307595+shivangi3001@users.noreply.github.com>
2022-06-10 00:45:23 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+
+import Header from './components/header/header';
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
