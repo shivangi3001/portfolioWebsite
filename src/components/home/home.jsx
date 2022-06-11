6858f6b96ce91bb43321282ff96c71ecdeafc1f8
Shivangi Chaurasia <56307595+shivangi3001@users.noreply.github.com>
2022-06-11 20:26:53 +0530

    changes in home page

---
+++ src/components/home/home.jsx
@@ -0,0 +1,13 @@
+import './home.css';
+import img from '../props/img2.jpg';
+import {BsMouse} from 'react-icons/bs' ;
+
+
+function Home() {
+    return (
+        <div id='home' className="container home-container">
+            <div className="logo">
+            </div>
+        </div>
+    )
+}
---
+++ src/components/nav/nav.jsx
@@ -0,0 +1,44 @@
+import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {TiOutlineHome} from 'react-icons/ti'
+import {BiMessageRoundedDots} from 'react-icons/bi'
+import {BsArrowDownCircle} from 'react-icons/bs'
+
+function Navbar (){
+    return(
+        <div className="navigation">
+            <a href="#home">
+                <AiOutlineHome className='icon active-nav' />
+            </a>
+            <a href="#about">
+                <AiOutlineUser className='icon' />
+            </a>
+            <a href="#members">
+                <TiGroupOutline className='icon' />
+            </a>
+            <a href="#contact">
+                <BiMessageRoundedDots className='icon' />
+            </a>
+            <a href="#home">
+                <BsArrowDownCircle className='icon' />
+            </a>
+        </div>
+    )
+}
+
+let Icons = document.querySelectorAll('.navigation .icon');
+Icons.forEach(icon => {
+    icons.addEventListener('click', () => {
+        changeactive();
+        icon.classList.add('active-nav');
+    });
+});
+
+function changeactive() {
+    Icons.forEach(icon => {
+        icon.classList.remove('active-nav')
+    })
+}
+
+export default Navbar;
