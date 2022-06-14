7b1f555ceb0243fb472d2bd4918f9faacd449407
Shivangi Chaurasia <56307595+shivangi3001@users.noreply.github.com>
2022-06-14 23:08:53 +0530

    changes in about section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 
 import Header from './components/header/header';
 import Navbar from './components/nav/nav';
+import Home from './components/home/home';
+import About from './components/about/about';
+// import Home from './components/home/home';
 
 import './App.css';
 
---
+++ src/components/about/about.jsx
@@ -0,0 +1,9 @@
+import './about/css'
+
+function  About() {
+    return (
+        <div id='about' className="container about-container">
+
+        </div>
+    )
+}
