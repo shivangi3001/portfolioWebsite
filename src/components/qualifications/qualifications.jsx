84f56af14554fa40cb39bdacddf86a0da32b8df2
Shivangi Chaurasia <56307595+shivangi3001@users.noreply.github.com>
2022-06-16 21:15:17 +0530

    about done

---
+++ src/components/qualifications/qualifications.jsx
@@ -0,0 +1,46 @@
+import './qualification.css'
+
+const Qualification = () => {
+    return (
+        <div>
+            <span className="quali-text">
+                My Qualifications
+            </span>
+            <div className="qualifications-container">
+                <div className = "qualification">
+                    <div className="content html">
+                    </div>
+                    <h1> html </h1>
+                </div>
+
+                <div className = "qualification">
+                    <div className="content css">
+                    </div>
+                    <h1> CSS </h1>
+                </div>
+
+                <div className = "qualification">
+                    <div className="content javascript">
+                    </div>
+                    <h1> javascript </h1>
+                </div>
+
+                <div className = "qualification">
+                    <div className="content bootstrap">
+                    </div>
+                    <h1> bootstrap </h1>
+                </div>
+
+                <div className = "qualification">
+                    <div className="content react">
+                    </div>
+                    <h1> react </h1>
+                </div>
+
+
+            </div>
+        </div>
+    );
+};
+
+export default Qualification;
